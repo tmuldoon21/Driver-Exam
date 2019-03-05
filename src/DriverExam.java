9428eda62f47ca708d1c94315c2ce2b859bd722b
tmuldoon21 <42975519+tmuldoon21@users.noreply.github.com>
2019-03-05 12:41:22 -0500

    Add files via upload

---
+++ src/DriverExam.java
@@ -0,0 +1,16 @@
+/**
+ *                   
+ * @author Tyler Muldoon
+ *Driver Exam
+ */
+public class DriverExam 
+{
+	private char[20] correctAnswers;
+	
+	public correctAnswers(char[] ca)
+	{
+		correctAnswers = ca;
+		char[] correctAnswers = {B, D, A, A, C, A, B, A, C, D, B, C, D, A, D, C, C, B, D, A};
+		
+	}
+}
